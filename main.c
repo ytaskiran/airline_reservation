0535b38bfda1a00ae3521fe813e3931366e89630
Yusuf <ytaskiran70@gmail.com>
2021-01-08 01:18:29 +0300

    0.0.3

--- main.c
+++ main.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
+int mainMenu();
 int passwordCheck();
 
 int main() {
     int choice, check;
-    system("clear");
 
-    printf( "\n\n**********************************************************************************\n" \
-            "**********************************************************************************\n" \
-            "**********\t\t\t\t\t\t\t\t**********\n" \ 
-            "**********\tWELCOME TO THE PISMANIYE AIRLINE RESERVATION SYSTEM\t********** \n" \
-            "**********\t\t\t\t\t\t\t\t**********\n" \ 
-            "**********************************************************************************\n" \
-            "**********************************************************************************\n" \
-            "\n\n\t1- Admin Menu\n" \
-            "\t2- Passenger Menu\n\n\n ");
-
-    printf("Select your menu: ");
-    scanf("%d", &choice);
+    choice = mainMenu();
 
     if (choice == 1){
-        check = passwordCheck();
-        printf("%d\n", check);
+        int count = 0;
+        do { 
+            check = passwordCheck();
+            if (check == 0){
+                printf("Password is correct\n");
+                break;
+            }
+            else {
+                count++;
+                if (count == 5){
+                    printf("\nToo many wrong attempts\n\n");
+                    exit(0);
+                }
+                else { 
+                    printf("Password is not correct, please try again.\n\n");
+                }
+            }} 
+            while (true);
+        
     }
 
     else if (choice == 2){
@@ -38,6 +45,27 @@ int main() {
 
 }
 
+int mainMenu() 
+{   
+    int choice;
+    system("clear");
+
+    printf( "\n\n**********************************************************************************\n");
+    printf( "**********************************************************************************\n");
+    printf( "**********\t\t\t\t\t\t\t\t**********\n");
+    printf( "**********\tWELCOME TO THE PISMANIYE AIRLINE RESERVATION SYSTEM\t********** \n");
+    printf( "**********\t\t\t\t\t\t\t\t**********\n");
+    printf( "**********************************************************************************\n");
+    printf( "**********************************************************************************\n");
+    printf( "\n\n\t1- Admin Menu\n");
+    printf( "\t2- Passenger Menu\n\n\n ");
+
+    printf("Select your menu: ");
+    scanf("%d", &choice);
+
+    return choice;
+}
+
 int passwordCheck()
 {
     char real_pw[20] = "admin123";
@@ -46,9 +74,6 @@ int passwordCheck()
 
     printf("\nPassword: ");
     scanf("%s", &input_pw);
-
-    printf("Input password: %s\n", input_pw);
-    printf("Real password: %s\n", real_pw); 
 
     check = strcmp(input_pw, real_pw);
 
